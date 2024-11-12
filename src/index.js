047406a159243e60543e8270b8c8fa3371a16d09
Glazzier <adrialopezfernandez@gmail.com>
2024-11-12 21:55:21 +0100

    discord bot base

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const config = require('../config.json')
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.Reaction],
+
+})
+
+client.on('ready', () => {
+  console.log('Bot is ready!')
+})
+
+client.login(config.token)
